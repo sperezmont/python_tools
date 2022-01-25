e955c29e5ecb50b47d42accda04445b31d5bebfb
Sergio Pérez Montero <98036930+sperezmont@users.noreply.github.com>
2022-01-25 10:43:34 +0100

    mk_cmap mode 'power'

--- colormaps/cmap_tools.py
+++ colormaps/cmap_tools.py
@@ -4,18 +4,24 @@
   Aim: Some functions for dealing with custom colormaps
 '''
 
-def mk_cmap(nportions, values, ncolors, mode='linear'):
+def mk_cmap(nportions, values, ncolors, mode='linear', deg=2):
     '''
     Generates a color map splitting the colorbar into 'nportions' with colors='values' with a lenght of ncolors
-    >>> value   value   value   value ...  len(values)
-    >>>   |portion|portion|portion|   ...  nportions
-    >>> \n
     >>> nportions = len(values) - 1
+    >>> ncolors > nportions
     >>> values = [[red, green, blue],...] in percentage units
-    >>> mode, gradation mode, default is 'linear'
+    >>> mode, gradation mode, default is 'linear', 'power'
+    >>> default power mode is 2 --> ax**2 + bx + c
     '''
     import numpy as np
     import matplotlib as mpl
+
+    if nportions > len(values)-1:
+        raise ValueError('nportions must be equal to len(values) - 1')
+
+    if ncolors < nportions:
+        raise ValueError('ncolors must be >= nportions')
+
     ncol_port = int(ncolors/nportions)
     cmap = []
     for i in range(nportions):
@@ -24,8 +30,19 @@ def mk_cmap(nportions, values, ncolors, mode='linear'):
             vals = []
             for j in range(3):
                 ini, fin = values[i][j], values[i+1][j]
-                if mode == 'linear':
+                if ini == fin:
+                    vals.append(np.array(ini))
+                elif mode == 'linear':
                     vals.append((fin - ini) * x / ncol_port + ini)
+                elif mode == 'power':
+                    xpoints = np.arange(0, ncol_port*(1+1/deg), ncol_port/deg)
+                    ypoints = np.arange(
+                        ini, fin + (fin-ini)/(len(xpoints)-1), (fin-ini)/(len(xpoints)-1))
+                    coef = np.polyfit(xpoints, ypoints, deg)
+                    term = 0
+                    for p in range(0, deg+1):
+                        term = term + coef[p]*x**(deg-p)
+                    vals.append(term)
             jcmap.append(vals)
         cmap.append(jcmap)
     cmap = np.array(cmap).reshape(nportions*ncol_port, 3)
